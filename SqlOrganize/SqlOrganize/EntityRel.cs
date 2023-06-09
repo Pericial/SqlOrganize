507bb836450c0d34abc88eb90273900905acab6f
Iván Castañeda <icastaneda@scba.gov.ar>
2023-06-09 10:56:39 -0300

    Incorporar clases iniciales para mapeo de json de la estructura de la base de datos. Por el momento los atributos se definen en snake_case. Posteriormente se analizara si se cambia a PascalCase para respetar la nomenclatura de c#

---
+++ SqlOrganize/SqlOrganize/EntityRel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlOrganize
+{
+    public class EntityRel
+    {
+        public string field_name { get; set; }
+        public string entity_name { get; set; }
+        public string field_ref_name { get; set; } = "id";
+        public string parent_id { get; set; }
+    }
+}
---
+++ SqlOrganize/SqlOrganize/EntityTree.cs
@@ -0,0 +1,14 @@
+﻿
+namespace SqlOrganize
+{
+    /*
+    Lectura de json
+    */
+    public class EntityTree
+    {
+        public string field_name { get; set; }
+        public string entity_name { get; set; }
+        public string field_ref_name { get; set; } = "id";
+        public Dictionary<string, EntityTree> children { get; set; }
+    }
+}
