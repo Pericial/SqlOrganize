e79f381a30d01ed358de269372c508dc6046c31e
unknown <ivancas84@gmail.com>
2023-07-13 03:21:18 -0300

    Reorganizar codigo

--- WinFormsAppMy/Data/AlumnoComision.cs
+++ WinFormsAppMy/Data/AlumnoComision.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utils;
 
 namespace WinFormsAppMy.Data
 {
@@ -22,5 +23,56 @@ namespace WinFormsAppMy.Data
                 Parameters("Mesa",anioCalendario,semestreCalendario).
                 ListDict();
         }
+
+        public static List<Dictionary<string, object>> InformeCoordinacionDistrital(string anioCalendario, int semestreCalendario)
+        {
+            var alumno_comision_ = AnioSemestreCalendario(anioCalendario, semestreCalendario);
+
+            foreach (Dictionary<string, object> alu_com in alumno_comision_)
+            {
+                alu_com["asignatura111"] = "";
+                alu_com["asignatura112"] = "";
+                alu_com["asignatura113"] = "";
+                alu_com["asignatura114"] = "";
+                alu_com["asignatura115"] = "";
+                alu_com["asignatura121"] = "";
+                alu_com["asignatura122"] = "";
+                alu_com["asignatura123"] = "";
+                alu_com["asignatura124"] = "";
+                alu_com["asignatura125"] = "";
+                alu_com["asignatura211"] = "";
+                alu_com["asignatura212"] = "";
+                alu_com["asignatura213"] = "";
+                alu_com["asignatura214"] = "";
+                alu_com["asignatura215"] = "";
+                alu_com["asignatura221"] = "";
+                alu_com["asignatura222"] = "";
+                alu_com["asignatura223"] = "";
+                alu_com["asignatura224"] = "";
+                alu_com["asignatura225"] = "";
+                alu_com["asignatura311"] = "";
+                alu_com["asignatura312"] = "";
+                alu_com["asignatura313"] = "";
+                alu_com["asignatura314"] = "";
+                alu_com["asignatura315"] = "";
+                alu_com["asignatura321"] = "";
+                alu_com["asignatura322"] = "";
+                alu_com["asignatura323"] = "";
+                alu_com["asignatura324"] = "";
+                alu_com["asignatura325"] = "";
+
+                var calificaciones = Calificacion.AprobadasPorAlumnoPlan((string)alu_com["alumno-id"], (string)alu_com["plan_alu-id"]);
+
+                foreach (Dictionary<string, object> calificacion in calificaciones)
+                {
+                    var nota = (!calificacion["nota_final"].IsNullOrEmpty() || (decimal)calificacion["nota_final"] >= 7) ? calificacion["nota_final"] : calificacion["crec"];
+                    string key = "asignatura" + calificacion["planificacion_dis-anio"].ToString() + calificacion["planificacion_dis-semestre"].ToString() + (string)calificacion["disposicion-orden_informe_coordinacion_distrital"].ToString();
+                    alu_com[key] = nota;
+                }
+            }
+
+            return new();
+        }
+
     }
 }
