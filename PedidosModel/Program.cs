725bf39955f5bcc46bf4b9f8d451dbcf9058fbec
Iván Castañeda <icastaneda@scba.gov.ar>
2023-08-30 08:48:40 -0300

    Agregar proyectos para administrar base de Pedidos

---
+++ PedidosModel/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using ModelOrganizeMy;
+using System.Configuration;
+
+var c = new ConfigMy()
+{
+    connectionString = ConfigurationManager.AppSettings.Get("connectionString"),
+    modelPath = ConfigurationManager.AppSettings.Get("modelPath"),
+    configPath = ConfigurationManager.AppSettings.Get("configPath"),
+    dbName = ConfigurationManager.AppSettings.Get("dbName"),
+    idSource = "field_name",
+};
+
+BuildModelMy t = new(c);
+
+t.CreateFileEntitites();
+
+t.CreateFileFields();
+
